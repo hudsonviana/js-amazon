89c68ab1f667ba4936d4b7b60d749580907266df
hudsonviana <hudson1206@gmail.com>
2023-10-23 13:55:24 -0300

    11. Install VS Code Extension

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -16,25 +16,27 @@ const HomeScreen = {
     const products = response.data;
 
     return `
-    <ul class="products">
-    ${products
-      .map(
-        (product) => `
-        <li>
-          <div class="product">
-            <a href="/#/product/${product._id}">
-              <img src="${product.image}" alt="${product.name}" />
-            </a>
-            <div class="product-name">
-              <a href="/#/product/${product._id}">${product.name}</a>
-            </div>
-            <div class="product-brand">${product.brand}</div>
-            <div class="product-price">$ ${product.price}</div>
-          </div>
-        </li>`
-      )
-      .join('\n')}
-    </ul>`;
+      <ul class="products">
+      ${products
+        .map(
+          (product) => `
+            <li>
+              <div class="product">
+                <a href="/#/product/${product._id}">
+                  <img src="${product.image}" alt="${product.name}" />
+                </a>
+                <div class="product-name">
+                  <a href="/#/product/${product._id}">${product.name}</a>
+                </div>
+                <div class="product-brand">${product.brand}</div>
+                <div class="product-price">$ ${product.price}</div>
+              </div>
+            </li>
+          `
+        )
+        .join('\n')}
+      </ul>
+    `;
   },
 };
 
