7069602bcc0f05b3aef965914dfff1393b8aed10
hudsonviana <hudson1206@gmail.com>
2023-10-24 20:27:47 -0300

    17. Design Cart UI

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -29,8 +29,60 @@ const CartScreen = {
         qty: 1,
       });
     }
-    return `<div>Cart Screen</div>
-    <div>${getCartItems().length}</div>`;
+
+    const cartItems = getCartItems();
+
+    return `
+    <div class="content cart">
+      <div class="cart-list">
+        <ul class="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+            ${
+              cartItems.length === 0
+                ? `<div>Cart is empty. <a href="/#/">Go shopping</a>.</div>`
+                : cartItems
+                    .map(
+                      (item) => `
+                      <li>
+                        <div class="cart-image">
+                          <img src="${item.image}" alt="${item.name}">
+                        </div>
+                        <div class="cart-name">
+                          <div>
+                            <a href="/#/product/${item.product}">${item.name}</a>
+                          </div>
+                          <div>
+                            Qty:
+                            <select id="${item.product}" class="qty-select">
+                              <option value="1">1</option>
+                              <option value="2">2</option>
+                            </select>
+                            <button class="delete-button" id="${item.product}">Delete</button>
+                          </div>
+                        </div>
+                        <div class="cart-price">
+                          $ ${item.price}
+                        </div>
+                      </li>`
+                    )
+                    .join('\n')
+            }
+  
+        </ul>
+      </div>
+      <div class="cart-action">
+        <h3>
+          Subtotal (${cartItems.reduce((a, c) => a + c.qty, 0)} items):
+          $ ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+        </h3>
+        <button id="checkout-button" class="primary fw">
+          Proced to checkout
+        </button>
+      </div>
+    </div>`;
   },
 };
 
